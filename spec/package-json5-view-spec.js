735f250496e21ec36d6d851bc9dc8ba6b9dc9f9a
Jim Neuendorf <jim.neuendorf@gmx.de>
2018-11-04 10:14:10 +0100

    initial commit

---
+++ spec/package-json5-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PackageJson5View from '../lib/package-json5-view';
+
+describe('PackageJson5View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
